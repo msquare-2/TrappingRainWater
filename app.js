3e03849f2709a91bc65d12072f7411ef2271909e
m4mustafa110 <m4mustafa110@gmail.com>
2022-10-25 21:05:10 +0530

    added styles

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
+// Function gets invoke on fill Water button click
+function fillWater() {
+  let buildingHeights = getBuildingHeights();
+  let waterHeights = getWaterHeights(buildingHeights);
+  let maxHeight = getMaxBuildingHeight(buildingHeights);
+  console.log(waterHeights);
+  let waterCount = tableCreate(buildingHeights, maxHeight, waterHeights);
+  let textNode = document.createTextNode(`${waterCount}`);
+  document.body.appendChild(textNode);
+}
+
+// Function to get building height and return it as array
 function getBuildingHeights() {
-  var heights = document.getElementById("heights").value;
+  let heights = document.getElementById("heights").value;
   let ans = [];
-  console.log(heights);
   for (const it of heights) {
     if (!isNaN(it)) {
         ans.push(parseInt(it));
@@ -10,22 +21,28 @@ function getBuildingHeights() {
   return ans;
 }
 
+// Function to calculate water height for perticular index
+// @parm building height array
+// @return calculated water heights
 function getWaterHeights(buildingHeights) {
-    var ans = [];
+    let ans = new Array(buildingHeights.length);
   for (let i = 0; i < buildingHeights.length; i++) {
     let leftmax = 0;
     let rightmax = 0;
     for (let l = i; l >= 0; l--) {
         leftmax = Math.max(leftmax,buildingHeights[l]);
     }
-    for (let r = i; r < array.length; r++) {
+    for (let r = i; r < ans.length; r++) {
         rightmax = Math.max(rightmax, buildingHeights[r]);
     }
-    ans.push(Math.min(leftmax, rightmax) - buildingHeights[i]);
+    ans[i] = Math.min(leftmax, rightmax) - buildingHeights[i];
   }
   return ans;
 }
 
+// Function to calculate max height amoung all buldings
+// @parm building height array
+// @return calculated max heights
 function getMaxBuildingHeight(buildingHeights) {
   let maxHeight = 0;
   buildingHeights.forEach((element) => {
@@ -34,15 +51,14 @@ function getMaxBuildingHeight(buildingHeights) {
   return maxHeight;
 }
 
-function fillWater() {
-  // var heights = document.getElementById("heights")[0].value;
-  var buildingHeights = getBuildingHeights();
-  var waterHeights = getWaterHeights(buildingHeights);
-  var maxHeight = getMaxBuildingHeight(buildingHeights);
-  tableCreate(buildingHeights, maxHeight, waterHeights);
-}
 
+// Function to create table graphics of building and water
+// @parm building height array
+// @parm max height of all buildings
+// @parm water height array
+// @return calculated total water units traped
 function tableCreate(buildingHeights, maxHeight, waterHeights) {
+  let waterCount = 0;
   const body = document.body,
     tbl = document.createElement("table");
     tbl.setAttribute('id', 'waterTable');
@@ -51,9 +67,10 @@ function tableCreate(buildingHeights, maxHeight, waterHeights) {
     const tr = tbl.insertRow();
     for (let j = 0; j < n; j++) {
       const td = tr.insertCell();
-      td.appendChild(document.createTextNode("cscnxc"));
+      td.appendChild(document.createTextNode(""));
       td.style.webkitTextFillColor = "white";
       td.style.height = "50px";
+      td.style.width = "50px";
       td.setAttribute("id", `${i},${j}`);
       if (i >= maxHeight - buildingHeights[j]) {
         td.style.backgroundColor = "Brown";
@@ -62,9 +79,10 @@ function tableCreate(buildingHeights, maxHeight, waterHeights) {
       else if (i >= maxHeight - buildingHeights[j] - waterHeights[j]) {
         td.style.backgroundColor = "Blue";
         td.style.webkitTextFillColor = "Blue";
+        waterCount = waterCount + 1;
       }
     }
   }
   body.appendChild(tbl);
+  return waterCount;
 }
-tableCreate([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1], 3, [0,0,1,0,1,2,1,0,0,1,0,0]);
